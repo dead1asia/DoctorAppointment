d74a0c88571e6dc239e4c6b43a2864a4f9bbebca
Енотович :DD <dead1asia@gmail.com>
2025-11-19 13:55:40 +0200

    Add project files.

---
+++ DoctorAppointment.Data/Configuration/Constants.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MyDoctorAppointment.Data.Configuration
+{
+    public static class Constants
+    {
+        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь) C:\\Users\\Enotovich\\Desktop\\Project VS\\DoctorAppointment\\
+        public const string AppSettingsPath = "..\\..\\..\\..\\DoctorAppointment.Data\\Configuration\\appsettings.json";
+    }
+}
